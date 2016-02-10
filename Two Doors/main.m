a8ecc1a45ed876e9088e6e617975cae330a34eb6
neliSon <[nelsonchow91.nc@gmail.com]>
2016-02-09 23:40:44 -0800

    Initial Commit

---
+++ Two Doors/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Two Doors
+//
+//  Created by Nelson Chow on 2016-02-09.
+//  Copyright © 2016 Nelson Chow. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
