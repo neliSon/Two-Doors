99630d0de7e7a998f02fab5b9e81995538b84ede
neliSon <[nelsonchow91.nc@gmail.com]>
2016-03-02 08:29:33 -0800

    Two Doors

--- Two Doors/main.m
+++ Two Doors/main.m
@@ -6,12 +6,60 @@
 //  Copyright © 2016 Nelson Chow. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+#include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+    
+    char play;
+    int choice;
+    
+    printf("Welcome to Two doors.\n");
+    printf("Would you like to play? (y/n): ");
+    scanf("%s", &play);
+    
+    if (play == 'y') {
+        
+        int count = 0;
+        char replay = 'y';
+        
+        while (count < 1) {
+            
+        printf("\nYou are a prisoner in a room with 2 doors and 2 guards.\n");
+        printf("One of the doors will guide you to freedom and behind the other is a hangman --you don't know which is which.\n");
+        printf("One of the guards always tells the truth and the other always lies. You don't know which one is the truth-teller or the liar either.\n");
+        printf("You have to choose and open one of these doors, but you can only ask a single question to one of the guards.\n");
+        printf("What do you ask so you can pick the door to freedom?\n\n");
+        printf("\t1.Ask the truth-guard to point to the door of doom.\n");
+        printf("\t2.Ask the liar-guard to point to the door of doom.\n");
+        printf("\t3.Doesn't matter which one you pick.\n");
+        scanf("%d", &choice);
+        
+        char *answer = "No matter which one you choose the guards both tell you which door leads to death, and therefore you can pick the other door.\n";
+        switch (choice) {
+            case 1:
+                printf("%s", answer);
+                break;
+            case 2:
+                printf("%s", answer);
+                break;
+            case 3:
+                printf("%s", answer);
+                break;
+            default:
+                break;
+            }
+            printf("Would you like to play agian? (y/n):\n");
+            scanf("%s", &replay);
+            
+            if (replay == 'y') {
+                count = 0;
+            }
+            if (replay == 'n') {
+                printf("Thanks for playing!\n");
+                break;
+            }
+        }
     }
-    return 0;
+    
+    return 1;
 }
